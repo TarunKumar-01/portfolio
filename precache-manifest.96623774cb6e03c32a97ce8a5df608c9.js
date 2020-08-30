a9d0355a0b4f8dfec1ab8d513e210a97a1a0102e
tarunk99 <tarunk17899@gmail.com>
2020-08-30 23:09:47 +0530

    Updates

---
+++ precache-manifest.96623774cb6e03c32a97ce8a5df608c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a6cd875025e1a19352a72a251a1fc4a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "81d7d617d67b461acecc",
+    "url": "/portfolio/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "395e66e7e415d4965393",
+    "url": "/portfolio/static/css/main.180ac5be.chunk.css"
+  },
+  {
+    "revision": "81d7d617d67b461acecc",
+    "url": "/portfolio/static/js/2.9d605e7e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.9d605e7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "395e66e7e415d4965393",
+    "url": "/portfolio/static/js/main.157c9ddf.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
